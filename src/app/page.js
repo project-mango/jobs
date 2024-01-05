62273832e6660b3cbdf08b46c1728ba8d87b69dd
= <singh.ashish1998@gmail.com>
2024-01-05 15:30:31 -0500

    supabase email auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 // import Image from 'next/image'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import styles from './page.module.css'
 import styles from './styles/projectMangoStyle.module.css';
 
