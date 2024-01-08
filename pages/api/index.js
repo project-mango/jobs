26c92af04b2dab0f1a8f16a95b020d743ca3a586
= <singh.ashish1998@gmail.com>
2024-01-08 09:41:45 -0500

    handle apply job's post call

--- pages/api/index.js
+++ pages/api/index.js
@@ -8,13 +8,16 @@ export default async function handler(req, res) {
     console.log("hello");
   if (req.method === 'POST') {
         try {
-            //console.log("huh")
+            console.log(1)
             const resumeUrl = applicantData.resume_link; // URL of the resume
            //console.log(resumeUrl)
+           console.log(1)
             const downloadFolder = './temp'; // Folder for the downloaded file
             const resumeFilePath = await downloadResume(resumeUrl, downloadFolder);
+            console.log(1)
             //console.log(resumeFilePath);
            await applyToJobs(resumeFilePath);
+           console.log(1)
            //console.log(123123)
             res.status(200).json({ message: 'Job applications started' });
         } catch (error) {
